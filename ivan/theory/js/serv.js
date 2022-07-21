6b6111172245b465ef23785dc036cc4c644f62a2
Misha <selmisha10@gmail.com>
2022-07-21 15:38:16 +0600

    added theory about errors

--- ivan/theory/js/serv.js
+++ ivan/theory/js/serv.js
@@ -205,7 +205,7 @@ const personeSecond = {
 console.log(personeSecond.userAge = 31);
 console.log(personeSecond.userAge);
 
-!Инкапсуляция
+//!Инкапсуляция
 
 function User(name, age) {
    this.name = name;
@@ -289,3 +289,52 @@ const misha = new User('misha', 20);
 misha.surname = 'Roga'
 misha.say();
 console.log(misha.surname);
+
+//!Ошибки
+try {
+   console.log('Ok');
+   console.log(a);
+} catch (error) {
+   console.log(error.name);
+   console.log(error.message);
+   console.log(error.stack);
+} finally {
+   console.log('Finally');
+}
+
+console.log('end');
+
+const data = [
+   {
+      id: 'box',
+      tag: 'div'
+   },
+   {
+      id: '',
+      tag: 'nav'
+   },
+   {
+      id: 'circle',
+      tag: 'span'
+   }
+]
+
+
+
+try {
+   data.forEach((blockObj, idx) => {
+      const block = document.createElement(blockObj.tag);
+      if (!blockObj.id) throw new SyntaxError(`В данных под номером ${idx + 1} нет id`);
+
+      block.setAttribute('id', blockObj.id);
+      document.body.append(block);
+   })
+} catch (error) {
+   if (error.name === 'SyntaxError') {
+      console.error(error.name);
+      console.error(error.message);
+      console.log(error.stack);
+   } else {
+      throw error;
+   }
+}
